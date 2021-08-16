c4d459221bd55738e79410fbfb34cc3feaa4f93d
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-08-16 18:09:42 +1000

    change time

--- animations.js
+++ animations.js
@@ -21,8 +21,8 @@ function setDate() {
   const minsDegrees = ((mins / 60) * 360) + 90;
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-  const hour = now.getMinutes();
-  const hourDegrees = ((mins / 12) * 360) + 90;
+  const hours = now.getHours();
+  const hourDegrees = ((hours / 12) * 360) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
