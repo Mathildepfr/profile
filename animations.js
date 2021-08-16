38ce073fbd7cf252db1b4029e0faf59e548ee4c6
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-08-16 18:06:51 +1000

    clock

--- animations.js
+++ animations.js
@@ -5,3 +5,25 @@ btnDis.addEventListener("click", (event) => {
   const audio = new Audio('files/sound.mp3');
   audio.play();
 });
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  let now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getMinutes();
+  const hourDegrees = ((mins / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
