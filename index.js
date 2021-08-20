f3a40bda0a8a4bf33a650bb3eb334cdf62f648d7
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-08-20 15:32:50 +1000

    text cloud

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { getTime } from "./animation/time.js";
 import { makeSound } from "./animation/sound.js";
 import { dropdownMenu } from "./animation/dropdown.js";
+import { textCloud } from "./animation/textCloud.js";
 
 getTime();
 makeSound();
 dropdownMenu();
+textCloud();
