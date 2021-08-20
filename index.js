ad4b53c2f52379c6cba06c66f942a1218d5ccaaf
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-08-20 14:26:40 +1000

    dropdown

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { getTime } from "./animation/time.js";
 import { makeSound } from "./animation/sound.js";
+import { dropdownMenu } from "./animation/dropdown.js";
 
 getTime();
 makeSound();
+dropdownMenu();
