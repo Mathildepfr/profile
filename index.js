0224aa076f027f38df89c6eae650d6352bdcc57a
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-09-04 16:38:29 +1000

    delete sound

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { getTime } from "./animation/time.js";
-import { makeSound } from "./animation/sound.js";
 import { dropdownMenu } from "./animation/dropdown.js";
 import { textCloud } from "./animation/textCloud.js";
 
 getTime();
-makeSound();
 dropdownMenu();
 textCloud();
